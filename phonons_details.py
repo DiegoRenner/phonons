8640c59e3be6668ec0fabc3180cd5f1bad22c762
drenner <drenner@cscs.ch>
2019-06-20 09:22:09 +0000

    CP2K input can now be generated for all DFT cases.

--- phonons_details.py
+++ phonons_details.py
@@ -27,7 +27,7 @@ layout = {'width': '70%'}
 layout3 = {'width': '23%'}
 
 class PHNSDetails(ipw.VBox):
-    def __init__(self,job_details={}, proc_rep=12, num_rep=1,  **kwargs):
+    def __init__(self,job_details={},  **kwargs):
         """ Dropdown for DFT details
         """
         ### ---------------------------------------------------------
@@ -35,15 +35,20 @@ class PHNSDetails(ipw.VBox):
         
         self.job_details=job_details
         
-     
+        self.proc_node = ipw.IntText(value=job_details['proc_node'],
+                           description='# Processors per Node',
+                           style=style, layout=layout)
 
-        self.proc_rep = ipw.IntText(value=proc_rep,
+        self.proc_rep = ipw.IntText(value=job_details['nproc_rep'],
                            description='# Processors per replica',
                            style=style, layout=layout)  
         
-        self.num_rep = ipw.IntText(value=num_rep,
-                           description='# replicas',
+        self.num_rep = ipw.IntText(value=job_details['nreplicas'],
+                           description='# Replicas',
                            style=style, layout=layout)
+        
+        
+        
         
         
         ### ---------------------------------------------------------
@@ -52,7 +57,7 @@ class PHNSDetails(ipw.VBox):
         
         
         update_jd_widgets = [
-            self.proc_rep, self.num_rep 
+            self.proc_node, self.proc_rep, self.num_rep
         ]
         for w in update_jd_widgets:
             w.observe(lambda v: self.update_job_details(), 'value')
@@ -62,8 +67,9 @@ class PHNSDetails(ipw.VBox):
         ### Define the ipw structure and create parent VBOX
 
         children = [
+            self.proc_node,
             self.proc_rep, 
-            self.num_rep, 
+            self.num_rep,
             self.neb_out
         ]
             
@@ -71,15 +77,19 @@ class PHNSDetails(ipw.VBox):
         
     ####TO DO decide how to deal with UPDATE VS WFN retrieve           
     def update_job_details(self):
-             
+        
+        self.job_details['proc_node']=self.proc_node.value     
         self.job_details['nproc_rep']=self.proc_rep.value
         self.job_details['nreplicas']=self.num_rep.value
         
-
-    def reset(self,proc_rep=12, num_rep=1):  
         
+
+    def reset(self, proc_node=1, proc_rep=12, num_rep=1, num_calcs=6000):  
+        
+        self.nodes_geopt.value = proc_node
         self.proc_rep.value = proc_rep
         self.num_rep.value = num_rep
+        
         #self.job_details={}
         self.update_job_details()
         
--- phonons_work.py
+++ phonons_work.py
@@ -21,13 +21,14 @@ from ase.neighborlist import NeighborList
 from ase import Atoms
 from io import StringIO, BytesIO
 
-from apps.surfaces.widgets.get_cp2k_input_dev import Get_CP2K_Input
+from apps.phonons.get_cp2k_input_dev import Get_CP2K_Input
+from apps.surfaces.widgets import find_mol
 
-class GWWorkChain(WorkChain):
+class PhononsWorkchain(WorkChain):
 
     @classmethod
     def define(cls, spec):
-        super(GWWorkChain, cls).define(spec)
+        super(PhononsWorkchain, cls).define(spec)
         
         spec.input('parameters', valid_type    = ParameterData)
         spec.input('code'      , valid_type    = Code)
@@ -36,7 +37,7 @@ class GWWorkChain(WorkChain):
         
         spec.outline(
             #cls.print_test,
-            cls.run_gw #,
+            cls.run_phonons #,
 #            while_(cls.not_converged)(
 #                cls.run_gw_again
 #            ),
@@ -52,7 +53,7 @@ class GWWorkChain(WorkChain):
         self.report("cell_str: %s %s" % (self.inputs.cell_str, str(self.inputs.cell_str)))
 
     # ==========================================================================
-    def run_gw(self):
+    def run_phonons(self):
         self.report("Running CP2K GW")
         
         parameters_dict = self.inputs.parameters.get_dict()
@@ -66,7 +67,7 @@ class GWWorkChain(WorkChain):
         self.report("settings: "+str(inputs['settings'].get_dict()))
         
         future = submit(Cp2kCalculation.process(), **inputs)
-        return ToContext(gw_opt=Calc(future))
+        return ToContext(phonons_opt=Calc(future))
 
     # ==========================================================================
 #    def run_gw_again(self):
@@ -89,7 +90,7 @@ class GWWorkChain(WorkChain):
                           input_dict    = None):
 
         inputs = {}
-        inputs['_label'] = "gw_opt"
+        inputs['_label'] = "phonons_opt"
         inputs['code'] = code
         inputs['file'] = {}
 
@@ -100,19 +101,32 @@ class GWWorkChain(WorkChain):
         basis_f = SinglefileData(file='/project/apps/surfaces/Files/RI_HFX_BASIS_all')
         inputs['file']['ri_hfx_basis_all'] = basis_f
         
-        if 'IC' not in input_dict['gw_type']:
-            ic_plane_z = None
-        else:
-            ic_plane_z = cls.find_ic_plane_z(atoms, input_dict['ads_height'])
-            
-        spin_guess = cls.extract_spin_guess(structure)
-
-        molslab_f = cls.make_geom_file(atoms, "mol.xyz", spin_guess=spin_guess, ic_plane_z=ic_plane_z)
         
-        inputs['file']['input_xyz'] = molslab_f
+        molslab_f = cls.mk_aiida_file(atoms, "mol_on_slab.xyz")
+        inputs['file']['molslab_coords'] = molslab_f
+        first_slab_atom = None
+        calc_type = input_dict['calc_type']
+        
+        if calc_type != 'Full DFT':
+            
+            # Au potential
+            pot_f = SinglefileData(file='/project/apps/surfaces/slab/Au.pot')
+            inputs['file']['au_pot'] = pot_f
+            tipii = find_mol.get_types(atoms,0.1)
+            mol_atoms = np.where(tipii==0)[0].tolist()
+            #mol_indexes = find_mol.molecules(mol_atoms,atoms)
+            #print(atoms[mol_indexes])
+            #print(mol_indexes)
+            first_slab_atom = len(mol_atoms) + 1
+            mol_f = cls.mk_aiida_file(atoms[mol_atoms], "mol.xyz")
+            inputs['file']['mol_coords'] = mol_f
+            
+            if calc_type == 'Mixed DFTB':
+                walltime = 18000
         
         # parameters
         cell_ase = atoms.cell.flatten().tolist()
+        
         
         if 'cell' in input_dict.keys():
             if input_dict['cell'] == '' or input_dict['cell'] == None :
@@ -127,6 +141,10 @@ class GWWorkChain(WorkChain):
 #        machine_cores = remote_computer.get_default_mpiprocs_per_machine()
      
         #inp =     get_cp2k_input(input_dict = input_dict)
+        input_dict['machine_cores'] = input_dict['nproc_rep']*input_dict['nreplicas']
+        input_dict['first_slab_atom'] = first_slab_atom
+        input_dict['last_slab_atom'] = len(atoms)
+        
         inp = Get_CP2K_Input(input_dict = input_dict).inp                         
         
 
@@ -137,15 +155,15 @@ class GWWorkChain(WorkChain):
             inputs['parent_folder'] = input_dict['remote_calc_folder']
 
         inputs['parameters'] = ParameterData(dict=inp)
-
+        
         # settings
-        settings = ParameterData(dict={'additional_retrieve_list': ['*.pdb']})
+        settings = ParameterData(dict={'additional_retrieve_list': ['*.mol_vib']})
         inputs['settings'] = settings
 
         # resources
         inputs['_options'] = {
             'resources' : {'num_machines'             : input_dict['num_machines'],
-                           'num_mpiprocs_per_machine' : input_dict['num_mpiprocs_per_machine'],
+                           'num_mpiprocs_per_machine' : input_dict['proc_node'],
                            'num_cores_per_mpiproc'    : input_dict['num_cores_per_mpiproc']
                           },
             'max_wallclock_seconds': int(input_dict['walltime']),
@@ -155,76 +173,13 @@ class GWWorkChain(WorkChain):
     
     # ==========================================================================
     @classmethod
-    def make_geom_file(cls, atoms, filename, spin_guess=None, ic_plane_z=None):
-        # spin_guess = [[spin_up_indexes], [spin_down_indexes]]
-        
-        n_atoms = len(atoms)
-        
+    def mk_aiida_file(cls, atoms, name):
         tmpdir = tempfile.mkdtemp()
-        file_path = tmpdir + "/" + filename
-
-        orig_file = BytesIO()
-        atoms.write(orig_file, format='xyz')
-        orig_file.seek(0)
-        all_lines = orig_file.readlines()
-        comment = all_lines[1].strip()
-        orig_lines = all_lines[2:]
-        
-        modif_lines = []
-        for i_line, line in enumerate(orig_lines):
-            new_line = line
-            lsp = line.split()
-            if spin_guess is not None:
-                if i_line in spin_guess[0]:
-                    new_line = lsp[0]+"1 " + " ".join(lsp[1:])+"\n"
-                if i_line in spin_guess[1]:
-                    new_line = lsp[0]+"2 " + " ".join(lsp[1:])+"\n"
-            modif_lines.append(new_line)
-        
-        imag_lines = []
-        if ic_plane_z is not None:
-            image = atoms.copy()
-            image.positions[:, 2] = 2*ic_plane_z - atoms.positions[:, 2]
-            
-            imag_file = BytesIO()
-            image.write(imag_file, format='xyz')
-            imag_file.seek(0)
-            imag_lines = imag_file.readlines()[2:]
-
-            imag_lines = [r.split()[0]+"G "+" ".join(r.split()[1:])+"\n" for r in imag_lines]
-            
-            n_atoms = 2*len(atoms)
-        
-        final_str = "%d\n%s\n" % (n_atoms, comment) + "".join(modif_lines+imag_lines)
-
-        with open(file_path, 'w') as f:
-            f.write(final_str)
-        aiida_f = SinglefileData(file=file_path)
+        atoms_file_name = tmpdir + "/" + name
+        atoms.write(atoms_file_name)
+        atoms_aiida_f = SinglefileData(file=atoms_file_name)
         shutil.rmtree(tmpdir)
-        return aiida_f
-    
-    # ==========================================================================
-    @classmethod
-    def extract_spin_guess(cls, struct_node):
-        sites_list = struct_node.get_attrs()['sites']
-        
-        spin_up_inds = []
-        spin_dw_inds = []
-        
-        for i_site, site in enumerate(sites_list):
-            if site['kind_name'][-1] == '1':
-                spin_up_inds.append(i_site)
-            elif site['kind_name'][-1] == '2':
-                spin_dw_inds.append(i_site)
-        
-        return [spin_up_inds, spin_dw_inds]
-    
-    # ==========================================================================
-    @classmethod
-    def find_ic_plane_z(cls, atoms, ads_height):
-        au_image_plane_height = 1.42 # Kharche et al.
-        au_surf = np.mean(atoms.positions[:, 2]) - ads_height
-        return au_surf + au_image_plane_height
+        return atoms_aiida_f
         
 
     # ==========================================================================
